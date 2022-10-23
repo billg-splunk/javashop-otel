6f51b431ec19a6b43a13586134e2636d60060e4c
shabuhabs <shabuhabs@gmail.com>
2022-10-23 19:55:51 +0000

    updated data

--- db/sql/create_tables.sql
+++ db/sql/create_tables.sql
@@ -1,15 +1,13 @@
-CREATE TABLE ItemsForSale(
-   Title           VARCHAR(81) NOT NULL
+DROP TABLE IF EXISTS InstrumentsForSale;
+CREATE TABLE IF NOT EXISTS InstrumentsForSale(
+   ID              INTEGER  NOT NULL PRIMARY KEY 
+  ,Title           VARCHAR(140) NOT NULL
   ,Sub_title       VARCHAR(58) NOT NULL
   ,Price           VARCHAR(12) NOT NULL
   ,Instrument_Type VARCHAR(29) NOT NULL
   ,Condition       VARCHAR(4) NOT NULL
   ,Location        VARCHAR(33) NOT NULL
-  ,Description     VARCHAR(5502) NOT NULL
-  ,Post_URL        VARCHAR(114) NOT NULL PRIMARY KEY
-  ,Seller_name     VARCHAR(74) NOT NULL
+  ,Post_URL        VARCHAR(114) NOT NULL
   ,Seller_type     VARCHAR(14) NOT NULL
   ,published_date  VARCHAR(14) NOT NULL
 );
-
-
