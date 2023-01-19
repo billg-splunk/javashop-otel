0c67ee060808f2e5ca3c895584cb4e00e561784f
shabuhabs <shabuhabs@gmail.com>
2023-01-19 16:53:58 -0700

    made root cause problem harder to find without custom attributes

--- products/src/main/java/com/shabushabu/javashop/products/resources/ProductResource.java
+++ products/src/main/java/com/shabushabu/javashop/products/resources/ProductResource.java
@@ -17,6 +17,10 @@ import javax.ws.rs.QueryParam;
 import java.util.Optional;
 import java.util.Random;
 
+
+
+
+
 @Path("/products")
 @Produces(MediaType.APPLICATION_JSON)
 public class ProductResource {
@@ -32,78 +36,51 @@ public class ProductResource {
     public Response getAllProducts(@DefaultValue("California") @QueryParam("location") String location) {
     	  
     	// STEP X: All we know right now is somewhere in this function, latency was introduced.
+  
+    	myCoolFunction1(location);
+    	myCoolFunction2(location);
+    	myCoolFunction10(location);
+    	myCoolFunction13(location);
+    	myCoolFunction5(location);
+    	myCoolFunction6(location);
     	
-
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
+    	myCoolFunction7(location);
+    	myCoolFunction8(location);
+    	myCoolFunction9(location);
     	
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
+    	myCoolFunction10(location);
+    	myCoolFunction11(location);
+    	myCoolFunction12(location);
     	//something in HAYSTACK
     	//something in HAYSTACK
     	//something in HAYSTACK
     	//something in HAYSTACK
 
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
+    	myCoolFunction23("The Location", location, 0);
+    	myCoolFunction14(location);
+    	///myCoolFunction15(location);
+    	myCoolFunction12(location);
+    	myCoolFunction24("The Location", location, 2);
+    	myCoolFunction12(location);
+    	myCoolFunction13(location);
     	
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	//something in HAYSTACK
+    	myCoolFunction14(location);
+    	myCoolFunction11(location);
+    	myCoolFunction13(location);
+    	
+    	myCoolFunction23("The Location", location, 0);
+    	myCoolFunction24("The Location", location, 0);
+    	myCoolFunction25("The Location", location, 0);
     	//something in HAYSTACK
     	//something in HAYSTACK
     	//something in HAYSTACK
     	//something in HAYSTACK
 
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
-    	myCoolFunction();
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction11(location);
     	myCoolFunction();
     	myCoolFunction();
     	
@@ -124,7 +101,6 @@ public class ProductResource {
     	//something in HAYSTACK
     	//something in HAYSTACK
     	//something in HAYSTACK
-    	//something in HAYSTACK
 
     	myCoolFunction();
     	myCoolFunction();
@@ -133,17 +109,10 @@ public class ProductResource {
     	myCoolFunction();
     	myCoolFunction();
     	myCoolFunction();
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-    	//something in HAYSTACK
-
+    	
     	myCoolFunction();
     	myCoolFunction();
     	myCoolFunction();
-    	//something in HAYSTACK
     	//something in HAYSTACK
     	//something in HAYSTACK
     	//something in HAYSTACK
@@ -154,11 +123,50 @@ public class ProductResource {
     	myCoolFunction();
     	myCoolFunction();
     	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction();
+    	myCoolFunction();
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+
+    	myCoolFunction(location);
+    	myCoolFunction();
+    	myCoolFunction();
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+    	//something in HAYSTACK
+
+    	myCoolFunction();
+    	myCoolFunction();
+    	myCoolFunction();
+    	//something in HAYSTACK
 
     	myCoolFunction();
     	//something in HAYSTACK
-    	//needle in HAYSTACK
-    	myCoolFunction_withLocation(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
+    	myCoolFunction(location);
     	myCoolFunction();
     	//something in HAYSTACK
     	//something in HAYSTACK
@@ -234,32 +242,404 @@ public class ProductResource {
                 .entity(productService.getAllProducts())
                 .build();
     }
+    
+    private void myCoolFunction(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        Thread.sleep(sleepy);
+        
+        lookupLocation1(location);
+        
+        } catch (Exception e){
+      	  
+        }
+      }
   
     
-    private void myCoolFunction() {
+    private void myCoolFunction1(String location) {
       // Generate a FAST sleep of 0 time !
-      int sleepy = 0;
+      int sleepy = lookupLocation1(location);
       try{
       Thread.sleep(sleepy);
+      
       } catch (Exception e){
     	  
       }
     }
+    private int lookupLocation1(String location) {
+    	 int sleepy =4;
+    	 
+    	 return sleepy;
+		
+	}
+    
+    private int lookupLocation19(String location) {
+    	
+    	int sleepy = lookupLocation1(location);
+    	// Generate a FAST sleep of 0 time !
+         
+    	try{
+        	 Thread.sleep(sleepy);
+        	 
+        	 
+         } catch (Exception e){
+       	  
+         }
+		return sleepy;
+	}
 
-
-    private void myCoolFunction_withLocation(String location) {
-    		
-      if (location.equalsIgnoreCase("Colorado")) {
-    	  // Generate a SLOW sleep of Random time !
-    	  Random random = new Random();
-    	  int sleepy = random.nextInt(5000 - 3000) + 3000;
-    	  try{
-    		  Thread.sleep(sleepy);
-    	  } catch (Exception e){
-    	  
-    	  }
+	private void myCoolFunction2(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 3;
+        try{
+        	
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
       }
+	
+    private int lookupLocation3(String location) {
+    	 // Generate a FAST sleep of 0 time !
+        int sleepy = 2;
+      
+        try{
+        	Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+		return sleepy;
+		
+	}
+    private int lookupLocation4(String location) {
+    	 // Generate a FAST sleep of 0 time !
+        int sleepy = 2;
+        if (location.equalsIgnoreCase("California")) {
+        	sleepy=4;
+        }
+        try{	
+        	Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+		return sleepy;
+		
+	}
+    private int myCoolFunction5(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 2;
+        lookupLocation3(location);
+        try{
+        	Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+		return sleepy;
+      }
+    private void myCoolFunction6(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 3;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction7(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 4;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction8(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 8;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction9(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        	
+        	Thread.sleep(sleepy);
+        
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction10(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction11(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = locationLookup11(location);
+        try{
+        	
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction12(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction13(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction14(String location) {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    
+    private void myCoolFunction23(String loc, String location, int index){
+    	System.out.println("Location Index is ... " + index);
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        	System.out.println("Location Index is ... " + index);
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction24(String loc, String location, int index){
+    	System.out.println("Location Index is ... " + index);
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        	System.out.println("Location Index is ... " + index);
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction25(String loc, String location, int index){
+		System.out.println("Location Index is ... ");
+
+    	System.out.println("Location Index is ... " + index);
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        	System.out.println("Location Index is ... " + index);
+        	Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    
+    private void myCoolFunction() {
+		System.out.println("Location Index is ... ");
+		int sleepy1 =3;
+    	  Random random = new Random();
+
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction33() {
+        // Generate a FAST sleep of 0 time !
+		System.out.println("Location Index is ... ");
+		int sleepy1 =3;
+    	  Random random = new Random();
+
+        int sleepy = 0;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction34(String location) {
+		System.out.println("Location Index is ... ");
+		int sleepy1 =3;
+    	  Random random = new Random();
+    	 
+        // Generate a FAST sleep of 0 time !
+        int sleepy = locationLookup2(location);
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+    
+    private int locationLookup2(String location) {
+    	System.out.println("Location Index is ... ");
+    	int sleepy1 =3;
+    	 // Generate a FAST sleep of 0 time !
+        int sleepy = 1;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+		return 0;
+	}
+    
+    private int locationLookup21(String location) {
+    	System.out.println("Location Index is ... ");
+    	int sleepy1 =3;
+    	 // Generate a FAST sleep of 0 time !
+        int sleepy = 1;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+		return 0;
+	}
+    private int locationLookup22(String location) {
+    	System.out.println("Location Index is ... ");
+    	int sleepy1 =3;
+    	 // Generate a FAST sleep of 0 time !
+        int sleepy = 1;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+		return 0;
+	}
+    private int locationLookup23(String location) {
+    	System.out.println("Location Index is ... ");
+		int sleepy1 =3;
+    	 // Generate a FAST sleep of 0 time !
+        int sleepy = 1;
+        try{
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+		return 0;
+	}
+
+	private int locationLookup11(String location) { 
+		System.out.println("Location Index is ... ");
+		int sleepy = 1;
+    	if (location.equalsIgnoreCase("Colorado")) {
+      	  // Generate a FAST sleep of 0 time !
+      	  Random random = new Random();
+      	  try{
+      		 myCoolFunction234234234(
+      		 getMyInt(location));
+      	  
+      	  } catch (Exception e){
+      	  
+      	  }
+        }
+    	return sleepy;
+      }
+	
+	 private int locationLookup15(String location) {
+    	System.out.println("Location Index is ... ");
+		
+    	 // Generate a FAST sleep of 0 time !
+        int sleepy = 1;
+        try{
+        Thread.sleep(sleepy);
+        Thread.sleep(sleepy);
+        Thread.sleep(sleepy);
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+		return 0;
+	}
+	 
+	 
+	 private void myCoolFunction234234234(int myInt) {
+	    	// Generate a FAST sleep of 0 time !
+	    	Random sleepy = new Random();
+	    	try{
+	        if (999==myInt) 
+	        Thread.sleep(
+	        sleepy.nextInt(5000 - 3000)
+	        + 3000);
+	        } catch (Exception e){
+	       
+	        }
+	    		
+	        }
+	 
+    private int locationLookup16(String location) {
+    	System.out.println("Location Index is ... ");
+		
+    	 // Generate a FAST sleep of 0 time !
+        int sleepy = 1;
+        try{
+        Thread.sleep(sleepy);
+        Thread.sleep(sleepy);
+        Thread.sleep(sleepy);
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+		return 0;
+	}
+    private void myCoolFunction3333() {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        Thread.sleep(sleepy);
+        Thread.sleep(sleepy);
+        Thread.sleep(sleepy);
+        
+        } catch (Exception e){
+      	  
+        }
+      }
+    private void myCoolFunction433434() {
+        // Generate a FAST sleep of 0 time !
+        int sleepy = 0;
+        try{
+        Thread.sleep(sleepy);
+        Thread.sleep(sleepy);
+        } catch (Exception e){
+      	  
+        }
+      }
+   
+    
+    private int getMyInt(String location ) {
+    	return 999;
     }
+
+    
     
     @GET
     @Path("/healthcheck")
